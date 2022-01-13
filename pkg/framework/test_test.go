b0fe82b1d86a194b46e2deb4bb279c976b297e7c
Simon Baeumer <sbaumer@redhat.com>
2022-01-13 09:34:05 +0100

    fix formatting

--- pkg/framework/test_test.go
+++ pkg/framework/test_test.go
@@ -34,21 +34,21 @@ func TestFind(t *testing.T) {
 		query          []string
 		expectNotFound bool
 	}{
-		"with only root node":       {query: []string{"root test"}},
-		"with child test":           {query: []string{"root test", "child test"}},
-		"with nested child":         {query: []string{"root test", "child test", "child child test"}},
-		"with not existing nested":  {query: []string{"root test", "child test", "child child test", "does not exist"}, expectNotFound: true},
-		"with not existing root":    {query: []string{"root does not exist"}, expectNotFound: true},
-		"with another child":        {query: []string{"root test", "another child test"}},
-		"with another nested child": {query: []string{"root test", "another child test", "another child child test"}},
-		"with same name finds both": {query: []string{"root test", "same name"}},
+		"with only root node":                        {query: []string{"root test"}},
+		"with child test":                            {query: []string{"root test", "child test"}},
+		"with nested child":                          {query: []string{"root test", "child test", "child child test"}},
+		"with not existing nested":                   {query: []string{"root test", "child test", "child child test", "does not exist"}, expectNotFound: true},
+		"with not existing root":                     {query: []string{"root does not exist"}, expectNotFound: true},
+		"with another child":                         {query: []string{"root test", "another child test"}},
+		"with another nested child":                  {query: []string{"root test", "another child test", "another child child test"}},
+		"with same name finds both":                  {query: []string{"root test", "same name"}},
 		"with not existent root should not be found": {query: []string{"non-existent root test", "child test"}, expectNotFound: true},
 	}
 
 	for _, tt := range testCases {
 		results := suite.find(tt.query)
 		if tt.expectNotFound {
-			assert.Nil(t, results)
+			assert.Empty(t, results)
 		} else {
 			require.NotEmpty(t, results)
 			for _, result := range results {
