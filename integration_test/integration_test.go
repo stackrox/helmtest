88bb857935914ca0033785ee8f5d069b5a2c460a
Simon Bäumer <sbaumer@redhat.com>
2022-04-20 10:20:43 +0200

    Add integration test setup (#15)

---
+++ integration_test/integration_test.go
@@ -0,0 +1,30 @@
+package integration_test
+
+import (
+	"testing"
+
+	"github.com/stackrox/helmtest/pkg/framework"
+	"github.com/stretchr/testify/require"
+	"helm.sh/helm/v3/pkg/chart/loader"
+	"helm.sh/helm/v3/pkg/chartutil"
+)
+
+func TestHelmTestShouldSucceed(t *testing.T) {
+	l := framework.NewLoader("testdata/helmtest")
+	s, err := l.LoadSuite()
+	require.NoError(t, err)
+
+	chart, err := loader.Load("testdata/nginx-example")
+	require.NoError(t, err)
+
+	target := &framework.Target{
+		Chart: chart,
+		ReleaseOptions: chartutil.ReleaseOptions{
+			Name:      "nginx-lb",
+			Namespace: "loadbalancer",
+			IsInstall: true,
+		},
+	}
+
+	s.Run(t, target)
+}
